b142889852d3749c2ab755fe94ea2552a7c84158
Jose <ls.joseignacio@gmail.com>
2023-04-25 11:40:59 +0200

    add question form

--- src/pages/consultas/index.tsx
+++ src/pages/consultas/index.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer/_footer";
 import Header from "@/components/Header/_header";
 import {
   IQuestion,
+  createNewQuestion,
   getQuestionsFilter,
   getUserQuestions,
 } from "@/services/api";
@@ -10,21 +11,46 @@ import Question from "./_question";
 
 import styles from "@/styles/Consultas/Consultas.module.scss";
 import { UserContext } from "@/contexts/user";
+import Link from "next/link";
 
 const Forum = () => {
   const [query, setQuery] = useState("");
   const { userData }: any = useContext(UserContext);
   const [userQuestions, setUserQuestions] = useState([] as IQuestion[]);
-  const [searchQuestions, setSearchQuestions] = useState([] as IQuestion[]);
+  const [searchQuestions, setSearchQuestions] = useState(
+    getQuestionsFilter("")
+  );
+
+  const [formData, setFormData] = useState({
+    question: "",
+    subscribe: true,
+  } as {
+    question: string;
+    subscribe: boolean;
+  });
+
+  const handleFormData = (value: any, field: string) => {
+    setFormData((oldValue) => {
+      return { ...oldValue, [field]: value };
+    });
+  };
+
+  const handleSubmitForm = (e: any) => {
+    e.preventDefault();
+    if (!userData?.email) return;
+    createNewQuestion(userData.email, formData.question, formData.subscribe);
+    setFormData({
+      question: "",
+      subscribe: false,
+    });
+    setUserQuestions(getUserQuestions(userData?.email));
+    setSearchQuestions(getQuestionsFilter(query));
+  };
 
   const handleUserInput = (event: any) => {
     const newQuery = event.currentTarget.value;
     setQuery(newQuery);
-    if (newQuery !== "") {
-      setSearchQuestions(getQuestionsFilter(newQuery));
-    } else {
-      setSearchQuestions([]);
-    }
+    setSearchQuestions(getQuestionsFilter(newQuery));
   };
 
   useEffect(() => {
@@ -36,11 +62,48 @@ const Forum = () => {
       <main>
         <h2>Centro de Gestión de Consultas</h2>
 
-        <h3>Consulta nuestro FAQ</h3>
+        <span>
+          Consulta nuestro{" "}
+          <Link href="/faq" className="link">
+            FAQ
+          </Link>
+        </span>
 
-        {userData.email && (
-          <>
+        {userData.email ? (
+          <div className="separatedBlock">
             <h3>Mis preguntas</h3>
+            <form
+              className={styles.questionForm}
+              onSubmit={(e) => {
+                handleSubmitForm(e);
+              }}
+            >
+              <h4>Nueva consulta</h4>
+              <label>
+                <span>Escribe tu pregunta</span>
+                <textarea
+                  value={formData.question}
+                  onChange={(e) => {
+                    handleFormData(e.currentTarget.value, "question");
+                  }}
+                ></textarea>
+              </label>
+              <label className={styles.labelRow}>
+                <input
+                  type="checkbox"
+                  checked={formData.subscribe}
+                  onChange={(e) => {
+                    handleFormData(e.currentTarget.checked, "subscribe");
+                  }}
+                />
+                <span>
+                  Deseo recibir un aviso por email cuando mi pregunta sea
+                  contestada
+                </span>
+              </label>
+              <button>Enviar pregunta</button>
+            </form>
+            <h4>Mis consultas previas</h4>
             <div className={styles.questionList}>
               {userQuestions.map((question: IQuestion, i: number) => (
                 <Question
@@ -51,31 +114,43 @@ const Forum = () => {
                 ></Question>
               ))}
             </div>
-          </>
+          </div>
+        ) : (
+          <div className="separatedBlock">
+            <h3>Inicia sesión para realizar consultas</h3>
+            <span>
+              Inicia sesión con tu cuenta de google usando el botón de la barra
+              de navegación para realizar tus consultas y que te notifiquemos al
+              ser contestadas.
+            </span>
+          </div>
         )}
 
-        <h3>Buscador</h3>
-        <input
-          className={styles.queryInput}
-          value={query}
-          onInput={(e) => handleUserInput(e)}
-        />
-        <div className={styles.questionList}>
-          {searchQuestions.length ? (
-            searchQuestions.map((question: IQuestion, i: number) => (
-              <Question
-                key={i}
-                question={question.question}
-                answer={question.answer}
-              ></Question>
-            ))
-          ) : (
-            <p>
-              {query === ""
-                ? "Escribe en la caja para empezar a búscar"
-                : "Búsqueda sin resultados"}
-            </p>
-          )}
+        <div className="separatedBlock">
+          <h3>Buscador</h3>
+          <input
+            className={styles.queryInput}
+            value={query}
+            onInput={(e) => handleUserInput(e)}
+          />
+          <div className={styles.questionList}>
+            {searchQuestions.length ? (
+              searchQuestions.map((question: IQuestion, i: number) => (
+                <Question
+                  key={i}
+                  question={question.question}
+                  answer={question.answer}
+                  showAvatar={false}
+                ></Question>
+              ))
+            ) : (
+              <p>
+                {query === ""
+                  ? "Escribe en la caja para empezar a búscar"
+                  : "Búsqueda sin resultados"}
+              </p>
+            )}
+          </div>
         </div>
       </main>
       <Footer></Footer>
