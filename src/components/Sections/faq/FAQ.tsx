fba98ddf80efe38121b175a91f439b98f4ea073e
Jose <ls.joseignacio@gmail.com>
2023-05-04 13:57:21 +0200

    add spinner to faq questions when loading

--- src/components/Sections/faq/FAQ.tsx
+++ src/components/Sections/faq/FAQ.tsx
@@ -3,13 +3,17 @@ import Question from "../../Shared/Question";
 import { IQuestion, getFaqQuestionsRequest } from "@/services/api";
 
 import List from "@/components/Shared/List";
+import Spinner from "@/components/Shared/Spinner";
 
 const FAQ = () => {
   const [faqQuestions, setFaqQuestions] = useState([] as IQuestion[]);
+  const [isLoadedQuestions, setIsLoadedQuestions] = useState(false);
   const getFaqQuestions = () => {
+    setIsLoadedQuestions(false);
     getFaqQuestionsRequest()
       .then((data) => {
         setFaqQuestions(data);
+        setIsLoadedQuestions(true);
       })
       .catch((err) => {
         console.log(err);
@@ -22,19 +26,22 @@ const FAQ = () => {
   return (
     <>
       <h2>Preguntas Frecuentes</h2>
-      <List
-        marginTop={true}
-        direction="column"
-      >{faqQuestions.map((question: IQuestion, i: number) => {
-        return (
-          <Question
-            key={question._id}
-            question={question.question}
-            answer={question.answer}
-            showAvatar={false}
-          ></Question>
-        );
-      })}</List>
+      <List marginTop={true} direction="column">
+        {isLoadedQuestions ? (
+          faqQuestions.map((question: IQuestion, i: number) => {
+            return (
+              <Question
+                key={question._id}
+                question={question.question}
+                answer={question.answer}
+                showAvatar={false}
+              ></Question>
+            );
+          })
+        ) : (
+          <Spinner></Spinner>
+        )}
+      </List>
     </>
   );
 };
